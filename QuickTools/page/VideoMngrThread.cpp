982b5ee3455b8bcd8386012a0a86a2f98fef07ed
pupuzi <87703143+pupuzi@users.noreply.github.com>
2024-04-13 18:29:43 +0800

    Add files via upload

--- QuickTools/page/VideoMngrThread.cpp
+++ QuickTools/page/VideoMngrThread.cpp
@@ -8,9 +8,21 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
+extern "C" {
+void log_callback(void* ptr, int level, const char* fmt, va_list vargs) {
+    Q_UNUSED(ptr);
+    Q_UNUSED(level);
+
+    char line[1024];
+    vsnprintf(line, sizeof(line), fmt, vargs);
+    qDebug() << line;
+}
+}
+
 VideoMngrThread::VideoMngrThread()
 {
     qDebug() << av_version_info();
+    //av_log_set_callback(log_callback);
 }
 
 VideoMngrThread::~VideoMngrThread()
@@ -18,167 +30,199 @@ VideoMngrThread::~VideoMngrThread()
 
 }
 
-bool VideoMngrThread::compress_h265(const QString& strName)
+void VideoMngrThread::printErrLog(int nRet)
+{
+    char error_buffer[AV_ERROR_MAX_STRING_SIZE];
+    av_strerror(nRet, error_buffer, sizeof(error_buffer));
+    emit sig_showLog(error_buffer);
+}
+
+bool VideoMngrThread::compress(const QString& strName)
 {
     QString strSrcName = strName;
-    QString strDestName = strName.mid(0, strName.lastIndexOf(".")) + "_h265.mp4";
+    QString strDestName = strName.mid(0, strName.lastIndexOf(".")) + "_c.mp4";
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     QByteArray utf8Dest = codec->fromUnicode(strDestName);
     QByteArray utf8Src = codec->fromUnicode(strSrcName);
     const char* outputFileName = utf8Dest;
     const char* inputFileName = utf8Src;
 
-    AVFormatContext* inputFormatCtx = nullptr;
-    AVFormatContext* outputFormatCtx = nullptr;
-    AVCodecContext* videoCodecContext = nullptr;
-    AVPacket* packet = av_packet_alloc();
+    emit sig_showLog("启动压缩: " + strName);
 
-    // 打开输入文件
-    avformat_open_input(&inputFormatCtx, inputFileName, nullptr, nullptr);
-    avformat_find_stream_info(inputFormatCtx, nullptr);
+    AVFormatContext* input_format_context = nullptr;
+    if(avformat_open_input(&input_format_context, inputFileName, nullptr, nullptr) != 0)
+    {
+        return false;
+    }
+    avformat_find_stream_info(input_format_context, NULL);
 
-    // 创建输出文件上下文
-    avformat_alloc_output_context2(&outputFormatCtx, nullptr, nullptr, outputFileName);
-    if (!outputFormatCtx) {
-       printf("Could not create output format context\n");
-       return -1;
+    AVStream* video_stream = nullptr;
+    for(unsigned int i = 0; i < input_format_context->nb_streams; i++)
+    {
+        if(input_format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            video_stream = input_format_context->streams[i];
+            break;
+        }
     }
 
-    // 复制输入流的参数到输出流
-    AVCodecContext* codecCtx = nullptr;
-    int videoStreamIndex = -1;
-    for (unsigned int i = 0; i < inputFormatCtx->nb_streams; i++) {
-       AVStream* inStream = inputFormatCtx->streams[i];
-       AVStream* outStream = avformat_new_stream(outputFormatCtx, nullptr);
-       if (!outStream) {
-           printf("Could not create new stream\n");
-           return -1;
-       }
-
-       // 复制编解码器参数
-       outStream->codecpar->codec_type = inStream->codecpar->codec_type;
-       outStream->codecpar->codec_id = inStream->codecpar->codec_id;
-       outStream->codecpar->bit_rate = inStream->codecpar->bit_rate;
-       outStream->codecpar->width = inStream->codecpar->width;
-       outStream->codecpar->height = inStream->codecpar->height;
-       outStream->codecpar->format = inStream->codecpar->format;
-       outStream->codecpar->sample_aspect_ratio = inStream->codecpar->sample_aspect_ratio;
-       outStream->codecpar->channel_layout = inStream->codecpar->channel_layout;
-       outStream->codecpar->channels = inStream->codecpar->channels;
-       outStream->codecpar->sample_rate = inStream->codecpar->sample_rate;
-       outStream->codecpar->block_align = inStream->codecpar->block_align;
-       outStream->time_base = inStream->time_base;
-
-       // 如果流是视频流并且需要转换编码，则设置新的编解码器
-       if (inStream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO &&
-           inStream->codecpar->codec_id != AV_CODEC_ID_HEVC) {
-           AVCodec* codec = (AVCodec*)avcodec_find_encoder(AV_CODEC_ID_HEVC);
-           if (!codec) {
-               printf("Could not find HEVC encoder\n");
-               return -1;
-           }
-
-           videoStreamIndex = i;
-           codecCtx = avcodec_alloc_context3(codec);
-           avcodec_parameters_to_context(codecCtx, outStream->codecpar);
-           avcodec_open2(codecCtx, codec, nullptr);
-           outStream->codecpar->codec_id = AV_CODEC_ID_HEVC;
-       }
+    AVStream* audio_stream = nullptr;
+    for(unsigned int i = 0; i < input_format_context->nb_streams; i++)
+    {
+        if(input_format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
+        {
+            audio_stream = input_format_context->streams[i];
+            break;
+        }
     }
 
-    // 获取视频流
-    AVStream* videoStream = inputFormatCtx->streams[videoStreamIndex];
+    AVCodecParameters* video_codec_params = video_stream->codecpar;
+    AVCodecParameters* audio_codec_params = audio_stream->codecpar;
 
-    // 获取视频解码器
-    const AVCodec* videoCodec = avcodec_find_decoder(videoStream->codecpar->codec_id);
-    if (!videoCodec) {
-        avformat_close_input(&inputFormatCtx);
+    AVFormatContext* output_format_context = nullptr;
+    if(avformat_alloc_output_context2(&output_format_context, nullptr, nullptr, outputFileName) < 0)
+    {
         return false;
     }
 
-    // 创建并打开视频解码器上下文
-    videoCodecContext = avcodec_alloc_context3(videoCodec);
-    if (!videoCodecContext) {
-        avformat_close_input(&inputFormatCtx);
+    AVStream* output_video_stream = avformat_new_stream(output_format_context, nullptr);
+    AVStream* output_audio_stream = avformat_new_stream(output_format_context, nullptr);
+
+    avcodec_parameters_copy(output_video_stream->codecpar, video_codec_params);
+    avcodec_parameters_copy(output_audio_stream->codecpar, audio_codec_params);
+
+    if(avio_open(&output_format_context->pb, outputFileName, AVIO_FLAG_WRITE) < 0)
+    {
         return false;
     }
 
-    //视频流参数去填充上下文context
-    avcodec_parameters_to_context(videoCodecContext, videoStream->codecpar);
-    if (avcodec_open2(videoCodecContext, videoCodec, nullptr) < 0) {
-        avformat_close_input(&inputFormatCtx);
-        avcodec_free_context(&videoCodecContext);
+    if(avformat_write_header(output_format_context, nullptr) < 0)
+    {
         return false;
     }
 
-    // 打开输出文件
-    if (!(outputFormatCtx->oformat->flags & AVFMT_NOFILE)) {
-       avio_open(&outputFormatCtx->pb, outputFileName, AVIO_FLAG_WRITE);
+    //解码器
+    emit sig_showLog("创建解码器...");
+    AVCodec* video_decodec = (AVCodec*)avcodec_find_decoder(video_codec_params->codec_id);
+    AVCodecContext* video_codec_context = avcodec_alloc_context3(video_decodec);
+    avcodec_parameters_to_context(video_codec_context, video_codec_params);
+    avcodec_open2(video_codec_context, video_decodec, nullptr);
+
+    AVCodec* audio_decodec = (AVCodec*)avcodec_find_decoder(audio_codec_params->codec_id);
+    AVCodecContext* audio_codec_context = avcodec_alloc_context3(audio_decodec);
+    avcodec_parameters_to_context(audio_codec_context, audio_codec_params);
+    avcodec_open2(audio_codec_context, audio_decodec, nullptr);
+
+    //编码器
+    emit sig_showLog("创建编码器...");
+    AVCodec* video_encodec = (AVCodec*)avcodec_find_encoder(video_codec_params->codec_id);
+    AVCodecContext* video_encoder_context = avcodec_alloc_context3(video_encodec);
+    avcodec_parameters_to_context(video_encoder_context, video_codec_params);
+    video_encoder_context->time_base = video_stream->time_base;
+    video_encoder_context->bit_rate = video_encoder_context->bit_rate / 4;
+    if(video_codec_context->bit_rate < 500000)
+    {
+        video_encoder_context->bit_rate = 500000;
+    }
+    video_encoder_context->width = 1280;
+    video_encoder_context->height = 720;
+    avcodec_open2(video_encoder_context, video_encodec, NULL);
+
+    AVCodec* audio_encodec = (AVCodec*)avcodec_find_encoder(audio_codec_params->codec_id);
+    AVCodecContext* audio_encoder_context = avcodec_alloc_context3(audio_encodec);
+    avcodec_parameters_to_context(audio_encoder_context, audio_codec_params);
+    audio_encoder_context->time_base = audio_stream->time_base;
+    avcodec_open2(audio_encoder_context, audio_encodec, NULL);
+
+    //编解码写入
+    emit sig_showLog("开始压缩...");
+    AVFrame* video_frame = av_frame_alloc();
+    AVPacket* video_packet = av_packet_alloc();
+    AVFrame* audio_frame = av_frame_alloc();
+    AVPacket* audio_packet = av_packet_alloc();
+    if(!video_frame || !video_packet)
+    {
+        return false;
     }
 
-    // 写入文件头
-    avformat_write_header(outputFormatCtx, nullptr);
+    double duration = (double)video_stream->duration / av_q2d(video_stream->time_base);
 
-    // 读取数据包并写入输出文件
-    int64_t next_pts = 0;
-    AVFrame* frame = av_frame_alloc();
-    AVPacket* transcodedPacket = av_packet_alloc();
-    while (av_read_frame(inputFormatCtx, packet) >= 0) {
-       // 找到输出流中对应的流
-       AVStream *outStream = nullptr;
-       for (unsigned int i = 0; i < outputFormatCtx->nb_streams; i++) {
-           if (outputFormatCtx->streams[i]->codecpar->codec_type == packet->stream_index) {
-               outStream = outputFormatCtx->streams[i];
-               break;
-           }
-       }
-       if (!outStream) {
-           // 如果没有找到对应的输出流，则跳过该数据包
-           av_packet_unref(packet);
-           continue;
-       }
+    int64_t frame_number = 0;
+    int64_t frame_total = video_stream->nb_frames;
+    AVPacket packet;
+    while(av_read_frame(input_format_context, &packet) >= 0)
+    {
+        if(packet.stream_index == video_stream->index)
+        {
+            frame_number++;
+            if(avcodec_send_packet(video_codec_context, &packet) < 0)
+            {
+                return false;
+            }
 
-       // 如果是视频流并且需要转码，则进行转码
-       if (packet->stream_index == inputFormatCtx->streams[videoStreamIndex]->index &&
-           outStream->codecpar->codec_id == AV_CODEC_ID_HEVC) {
-           av_packet_unref(transcodedPacket);
-           avcodec_send_packet(videoCodecContext, packet);
+            while(avcodec_receive_frame(video_codec_context, video_frame) >= 0)
+            {
+                if(avcodec_send_frame(video_encoder_context, video_frame) < 0)
+                {
+                    return false;
+                }
 
-           while (avcodec_receive_frame(videoCodecContext, frame) == 0) {
+                while(avcodec_receive_packet(video_encoder_context, video_packet) >= 0)
+                {
+                    av_packet_rescale_ts(video_packet, video_encoder_context->time_base, output_video_stream->time_base);
+                    video_packet->stream_index = output_video_stream->index;
+                    av_interleaved_write_frame(output_format_context, video_packet);
+                    av_packet_unref(video_packet);
+                }
+            }
 
-               int64_t timebase_ticks_per_second = av_q2d(outputFormatCtx->streams[videoStreamIndex]->time_base) * 1000000;
-               // 设置输出帧的PTS
-               // pts_increment是每帧之间的时间增量，通常等于输入帧的PTS差值或帧率倒数
-               int64_t pts_increment = av_rescale_q(1, inputFormatCtx->streams[videoStreamIndex]->time_base, outputFormatCtx->streams[videoStreamIndex]->time_base);
-               frame->pts = next_pts;
-               next_pts += pts_increment;
+            //计算转码进度
+            //QString strDuration = QString("压制进度: %1 / %2").arg(frame_number).arg(frame_total);
+            //emit sig_showLog(strDuration);
+        }
+        else if(packet.stream_index == audio_stream->index)
+        {
+            if(avcodec_send_packet(audio_codec_context, &packet) < 0)
+            {
+                return false;
+            }
 
-               avcodec_send_frame(codecCtx, frame);
-               avcodec_receive_packet(codecCtx, transcodedPacket);
+            while(avcodec_receive_frame(audio_codec_context, audio_frame) >= 0)
+            {
+                if(avcodec_send_frame(audio_encoder_context, audio_frame) < 0)
+                {
+                    return false;
+                }
 
-               transcodedPacket->pts = frame->pts;
-               transcodedPacket->dts = frame->pts;
-               transcodedPacket->duration = pts_increment;
-               // 将转码后的数据包写入输出文件
-               av_interleaved_write_frame(outputFormatCtx, transcodedPacket);
-               av_packet_unref(transcodedPacket);
-           }
+                while(avcodec_receive_packet(audio_encoder_context, audio_packet) >= 0)
+                {
+                    av_packet_rescale_ts(audio_packet, audio_encoder_context->time_base, output_audio_stream->time_base);
+                    audio_packet->stream_index = output_audio_stream->index;
+                    av_interleaved_write_frame(output_format_context, audio_packet);
+                    av_packet_unref(video_packet);
+                }
+            }
+        }
 
-           av_packet_unref(packet);
-       } else {
-           // 如果是其他流或者不需要转码的视频流，则直接写入输出文件
-           av_interleaved_write_frame(outputFormatCtx, packet);
-       }
+        av_packet_unref(video_packet);
+        av_packet_unref(audio_packet);
     }
 
-    // 写入文件尾
-    av_write_trailer(outputFormatCtx);
+    av_write_trailer(output_format_context);
 
-    // 清理资源
-    avcodec_free_context(&codecCtx);
-    av_packet_free(&packet);
-    avio_close(outputFormatCtx->pb);
-    avformat_free_context(outputFormatCtx);
+    avcodec_free_context(&video_codec_context);
+    avcodec_free_context(&video_encoder_context);
+    av_frame_free(&video_frame);
+    av_packet_free(&video_packet);
+    avcodec_free_context(&audio_codec_context);
+    avcodec_free_context(&audio_encoder_context);
+    av_frame_free(&audio_frame);
+    av_packet_free(&audio_packet);
+    avformat_close_input(&input_format_context);
+    avio_closep(&output_format_context->pb);
+    avformat_free_context(output_format_context);
+
+    emit sig_showLog("压缩结束!");
 
     return true;
 }
@@ -201,6 +245,6 @@ void VideoMngrThread::run()
 {
     foreach(QString strName, m_strNames)
     {
-        compress_h265(strName);
+        compress(strName);
     }
 }
