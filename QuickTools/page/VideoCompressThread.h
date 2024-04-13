6f6a67b9c74002f2c8ced5ceb1b967e6ac9f5077
pupuzi <87703143+pupuzi@users.noreply.github.com>
2024-04-13 19:03:35 +0800

    Add files via upload

---
+++ QuickTools/page/VideoCompressThread.h
@@ -0,0 +1,31 @@
+﻿#ifndef VIDEOCOMPRESSTHREAD_H
+#define VIDEOCOMPRESSTHREAD_H
+
+#include <QThread>
+
+class VideoCompressThread : public QThread
+{
+    Q_OBJECT
+
+private:
+    QString m_strError;
+    QStringList m_strNames;
+
+    void printErrLog(int nRet);
+    bool compress(const QString& strName);
+
+public:
+    VideoCompressThread();
+    virtual ~VideoCompressThread();
+
+    void startThread(const QStringList& strNames);
+    void startThread(const QString& strName);
+
+protected:
+    void run() override;
+
+signals:
+    void sig_showLog(QString sigLog);
+};
+
+#endif // VIDEOMNGRTHREAD_H
---
+++ QuickTools/page/VideoMoveThread.h
@@ -0,0 +1,34 @@
+﻿#ifndef VIDEOMOVETHREAD_H
+#define VIDEOMOVETHREAD_H
+
+#include <QMap>
+#include <QThread>
+#include <QFileInfo>
+
+class VideoMoveThread : public QThread
+{
+    Q_OBJECT
+
+private:
+    QString m_strSource;
+    QString m_strDest;
+    QString m_strName;
+
+    QMap<QString, QFileInfo> m_listFileInfo;
+    void GetFileInfos(const QString& strFilePath);
+    QString GetCurrentLog(const QString& strLog);
+
+public:
+    VideoMoveThread();
+    virtual ~VideoMoveThread();
+
+    void startThread(const QString& strSource, const QString& strDest, const QString& strName);
+
+protected:
+    void run() override;
+
+signals:
+    void sig_showLog(QString sigLog);
+};
+
+#endif // FILEMOVETHREAD_H
